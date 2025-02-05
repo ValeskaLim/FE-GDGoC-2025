067fa4b030eb9c734c86b03c88e2a5d3d58c79ef
ValeskaLim <valeskavalentinekklesia@gmail.com>
2025-02-05 19:01:36 +0700

    Sesi 2 update

--- Sesi 2/to-do-list/src/App.tsx
+++ Sesi 2/to-do-list/src/App.tsx
@@ -2,17 +2,34 @@ import { useState } from "react";
 import Form from "./components/Form";
 import "./App.css";
 
-const App = ()  => {
-  // const [count, setCount] = useState(0);
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClicks = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
 
   return (
     <>
       <div className="mx-auto bg-gray-100 rounded-xl p-5">
         <h1 className="text-5xl">Form</h1>
         <Form />
+        <div className="bg-green-400 rounded-md w-fit">
+          <button
+            type="button"
+            onClick={count < 10 ? handleClicks : undefined}
+            className={`p-3 ${
+              count >= 10 ? "bg-gray-200 rounded-md" : "active:bg-green-700"
+            }`}
+            disabled={count >= 10}
+          >
+            Try click me: {count}
+          </button>
+        </div>
+        {count == 10 && <p className="text-red-500 font-bold">You clicked 10 times!</p>}
       </div>
     </>
   );
-}
+};
 
 export default App;
