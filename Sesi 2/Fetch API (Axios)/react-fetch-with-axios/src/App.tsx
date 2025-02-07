4769bb3701ddd1ee7c005fd4903b8ce0e3d90d68
ValeskaLim <valeskavalentinekklesia@gmail.com>
2025-02-07 18:05:13 +0700

    finalize

--- Sesi 2/Fetch API (Axios)/react-fetch-with-axios/src/App.tsx
+++ Sesi 2/Fetch API (Axios)/react-fetch-with-axios/src/App.tsx
@@ -1,14 +1,22 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+interface Channel {
+  id: string;
+  name: string;
+  group: string;
+  photo: string;
+  subscriber_count: string;
+}
+
 function App() {
-  const [datas, setData] = useState([]);
+  const [datas, setData] = useState<Channel[]>([]);
 
   const API_KEY = import.meta.env.VITE_API_KEY;
 
   useEffect(() => {
     axios
-      .get("https://holodex.net/api/v2/channels", {
+      .get<Channel[]>("https://holodex.net/api/v2/channels", {
         params: {
           org: "Hololive",
           lang: "en",
@@ -22,10 +30,10 @@ function App() {
       })
       .then((response) => {
         const filteredData = response.data.filter(
-          (channel) =>
-            channel.group && channel.group.toLowerCase().trim().includes("english")
+          (channel: Channel) =>
+            channel.group && channel.group.toLowerCase().trim().includes("")
         );
-        console.log(response.data);
+        console.log(filteredData);
         setData(filteredData);
       })
       .catch((error) => {
@@ -37,16 +45,27 @@ function App() {
     <>
       <div className="text-center min-h-screen flex flex-col justify-center mt-5">
         <h1 className="text-blue-600 text-5xl">
-          Fetch API in React using Axios
+          Hololive English Channel List
         </h1>
-        <div className="grid grid-cols-3 gap-3 mt-5 mx-10">
+        <div className="grid grid-cols-3 gap-3 my-10 mx-50">
           {datas.map((data) => (
-            <div key={data.id} className="p-4 border rounded shadow">
-              <h2 className="text-xl font-bold">{data.name}</h2>
-              <p>{data.group}</p>
-              <img src={data.photo} />
-              <p>{data.lang}</p>
-              <p>{data.description}</p>
+            <div key={data.id} className="p-4 border rounded-xl shadow">
+              <h2 className="text-3xl font-bold">{data.name}</h2>
+              <p className="text-blue-500 font-bold text-xl">{data.group}</p>
+              <div className="w-fit m-auto rounded-3xl overflow-hidden mt-4">
+                <img
+                  src={data.photo}
+                  width={500}
+                  height={500}
+                  className="w-full h-full object-fit hover:scale-[1.05] transition ease-in-out duration-150"
+                />
+              </div>
+              <p className="text-2xl">
+                Subscribers:
+                <span className="font-bold text-red-500">
+                  {data.subscriber_count}
+                </span>
+              </p>
             </div>
           ))}
         </div>
